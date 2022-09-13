4aa9d76bc2146cf463d51f975a7518cc22a3a8d8
rex <81349047+cjsrkd3321@users.noreply.github.com>
2022-09-13 21:57:35 +0900

    Update cloudtrail s3 encryption settings and public access

--- ALL_REGION_EVENT_ALARM/cloudtrail.tf
+++ ALL_REGION_EVENT_ALARM/cloudtrail.tf
@@ -18,6 +18,26 @@ resource "aws_s3_bucket" "cloudtrail_s3" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.cloudtrail_s3.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.cloudtrail_s3.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+    bucket_key_enabled = true
+  }
+}
+
 resource "aws_s3_bucket_lifecycle_configuration" "s3_lifecycle" {
   bucket = aws_s3_bucket.cloudtrail_s3.bucket
 
