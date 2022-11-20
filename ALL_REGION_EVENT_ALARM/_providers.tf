11aa46c0f3be4423b82556f8a8a513e9e0c26695
rex <81349047+cjsrkd3321@users.noreply.github.com>
2022-11-20 13:36:19 +0900

    add tags

--- ALL_REGION_EVENT_ALARM/_providers.tf
+++ ALL_REGION_EVENT_ALARM/_providers.tf
@@ -1,86 +1,188 @@
 provider "aws" {
   region = "ap-northeast-1"
   alias  = "apne1"
+
+  default_tags {
+    tags = {
+      Owner = "rex.chun"
+    }
+  }
 }
 
 provider "aws" {
   region = "ap-northeast-2"
   alias  = "apne2"
+
+  default_tags {
+    tags = {
+      Owner = "rex.chun"
+    }
+  }
 }
 
 provider "aws" {
   region = "ap-northeast-3"
   alias  = "apne3"
+
+  default_tags {
+    tags = {
+      Owner = "rex.chun"
+    }
+  }
 }
 
 provider "aws" {
   region = "ap-south-1"
   alias  = "aps1"
+
+  default_tags {
+    tags = {
+      Owner = "rex.chun"
+    }
+  }
 }
 
 provider "aws" {
   region = "ap-southeast-1"
   alias  = "apse1"
+
+  default_tags {
+    tags = {
+      Owner = "rex.chun"
+    }
+  }
 }
 
 provider "aws" {
   region = "ap-southeast-2"
   alias  = "apse2"
+
+  default_tags {
+    tags = {
+      Owner = "rex.chun"
+    }
+  }
 }
 
 provider "aws" {
   region = "ca-central-1"
   alias  = "cac1"
+
+  default_tags {
+    tags = {
+      Owner = "rex.chun"
+    }
+  }
 }
 
 provider "aws" {
   region = "eu-central-1"
   alias  = "euc1"
+
+  default_tags {
+    tags = {
+      Owner = "rex.chun"
+    }
+  }
 }
 
 provider "aws" {
   region = "eu-north-1"
   alias  = "eun1"
+
+  default_tags {
+    tags = {
+      Owner = "rex.chun"
+    }
+  }
 }
 
 provider "aws" {
   region = "eu-west-1"
   alias  = "euw1"
+
+  default_tags {
+    tags = {
+      Owner = "rex.chun"
+    }
+  }
 }
 
 provider "aws" {
   region = "eu-west-2"
   alias  = "euw2"
+
+  default_tags {
+    tags = {
+      Owner = "rex.chun"
+    }
+  }
 }
 
 provider "aws" {
   region = "eu-west-3"
   alias  = "euw3"
+
+  default_tags {
+    tags = {
+      Owner = "rex.chun"
+    }
+  }
 }
 
 provider "aws" {
   region = "sa-east-1"
   alias  = "sae1"
+
+  default_tags {
+    tags = {
+      Owner = "rex.chun"
+    }
+  }
 }
 
 provider "aws" {
   region = "us-east-1"
   alias  = "use1"
+
+  default_tags {
+    tags = {
+      Owner = "rex.chun"
+    }
+  }
 }
 
 provider "aws" {
   region = "us-east-2"
   alias  = "use2"
+
+  default_tags {
+    tags = {
+      Owner = "rex.chun"
+    }
+  }
 }
 
 provider "aws" {
   region = "us-west-1"
   alias  = "usw1"
+
+  default_tags {
+    tags = {
+      Owner = "rex.chun"
+    }
+  }
 }
 
 provider "aws" {
   region = "us-west-2"
   alias  = "usw2"
+
+  default_tags {
+    tags = {
+      Owner = "rex.chun"
+    }
+  }
 }
 
 provider "aws" {
