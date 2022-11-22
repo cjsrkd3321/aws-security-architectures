71c39b69710e964348d6760bf3581174bf449412
rex.chun <81349047+cjsrkd3321@users.noreply.github.com>
2022-11-22 21:33:32 +0900

    Add IAMListUserGroupAttachments

---
+++ NUKE/resources/IAMListUserGroupAttachments.py
@@ -0,0 +1,77 @@
+from . import resources, Config
+from ._base import ResourceBase
+from ._utils import filter_func
+from .IAMUsers import IAMUser
+
+
+import boto3
+
+
+class IAMListUserGroupAttachment(ResourceBase):
+    def __init__(self, region="ap-northeast-2") -> None:
+        self.svc = boto3.client("iam", config=Config(region_name=region))
+        self.exceptions = self.svc.exceptions
+
+    def list(self):
+        try:
+            iam_user = IAMUser()
+            users, err = iam_user.list()
+            if err:
+                return [], err
+            filtered_users, err = iam_user.filter(users, filter_func)
+            if err:
+                return [], err
+
+            results = []
+            for user in filtered_users:
+                user_name = user["id"]
+                try:
+                    groups = self.svc.list_groups_for_user(UserName=user_name)
+                    if not groups:
+                        return [], None
+                    results += [
+                        {
+                            "id": group["GroupName"],
+                            "name": group["GroupName"],
+                            "user_name": user_name,
+                            "create_date": group["CreateDate"],
+                            "tags": [],
+                        }
+                        for group in groups["Groups"]
+                    ]
+                except self.exceptions.NoSuchEntityException:
+                    continue
+            return results, None
+        except Exception as e:
+            return [], e
+
+    def remove(self, resource):
+        try:
+            return (
+                self.svc.remove_user_from_group(
+                    UserName=resource["user_name"], GroupName=resource["id"]
+                )["ResponseMetadata"]["HTTPStatusCode"]
+                == 200
+            ), None
+        except self.exceptions.NoSuchEntityException:
+            return True, None
+        except Exception as e:
+            return False, e
+
+    def filter(self, resources, filter_func=None):
+        if not resources:
+            return [], None
+        filtered_resources = resources
+        if filter_func:
+            try:
+                filtered_resources = filter_func(filtered_resources)
+            except Exception as e:
+                return [], e
+        return filtered_resources, None
+
+    def properties(self):
+        pass
+
+
+if __name__ != "__main__":
+    resources.append(IAMListUserGroupAttachment)
