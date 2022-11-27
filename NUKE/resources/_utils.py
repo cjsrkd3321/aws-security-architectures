2a5d6fb82a291c3e9943421f17cbe51675d752e6
rex <81349047+cjsrkd3321@users.noreply.github.com>
2022-11-27 13:43:12 +0900

    Add timeit decorator function

--- NUKE/resources/_utils.py
+++ NUKE/resources/_utils.py
@@ -1,3 +1,7 @@
+from functools import wraps
+import time
+
+
 def get_name_from_tags(tags):
     if not tags:
         return None
@@ -5,3 +9,16 @@ def get_name_from_tags(tags):
         if tag.get("Key") == "Name":
             return tag.get("Value")
     return None
+
+
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f"Function {func.__name__} Took {total_time:.4f} seconds")
+        return result
+
+    return timeit_wrapper
