3882660c043ad04b5977fce5a173e2e6e2ac9406
rex <81349047+cjsrkd3321@users.noreply.github.com>
2022-11-27 23:24:17 +0900

    Add items

---
+++ NUKE/items/item.py
@@ -0,0 +1,39 @@
+from resources._base import ResourceState as rs
+
+
+class Item:
+    def __init__(self, resource, region, name, filterer, remover):
+        self.__item = {
+            "state": rs.New,
+            "reason": None,
+            "region": region,
+            "resource": resource,
+            "type": name,
+        }
+        self.__filterer = filterer
+        self.__remover = remover
+
+    def filter(self):
+        is_filter, err = self.__filterer(self.__item["resource"])
+        self.__item["state"] = rs.Filtered if is_filter else rs.New
+        self.__item["reason"] = is_filter if is_filter else err
+
+    def remove(self):
+        is_removed, err = self.__remover(self.__item["resource"])
+        if err:
+            print(f"{self.__item['type']} | {err}")
+        self.__item["state"] = rs.Removed if is_removed else rs.Failed
+        self.__item["reason"] = None if is_removed else err
+
+    def is_skip(self):
+        if self.__item["state"] in [rs.Filtered, rs.Removed]:
+            return True
+        return False
+
+    @property
+    def item(self):
+        return self.__item
+
+    @property
+    def current(self):
+        return f"{self.__item['region']} - {self.__item['type']} - {self.__item['resource']['id']} | {self.__item['state']} - {self.__item['reason']}"
