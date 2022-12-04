a1d0acdba8b65103c844ba94a9b82c6e6fdfb92d
rex <81349047+cjsrkd3321@users.noreply.github.com>
2022-12-04 19:13:38 +0900

    Add IAMOpenIdConnectProviders

--- NUKE/tests/iam.tf
+++ NUKE/tests/iam.tf
@@ -142,3 +142,12 @@ resource "aws_iam_instance_profile" "this" {
   name = "nuke-profile"
   role = aws_iam_role.this.name
 }
+
+# IAMOpenIdConnectProviders
+resource "aws_iam_openid_connect_provider" "this" {
+  url = "https://accounts.google.com"
+  client_id_list = [
+    "266362248691-342342xasdasdasda-apps.googleusercontent.com",
+  ]
+  thumbprint_list = []
+}
