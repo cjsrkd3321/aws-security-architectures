a43f84d9816ed7d3414ae8fccd6e93887521fe65
rex <81349047+cjsrkd3321@users.noreply.github.com>
2022-12-04 16:11:13 +0900

    Modify function name for lambda tests

--- NUKE/tests/lambda.tf
+++ NUKE/tests/lambda.tf
@@ -1,6 +1,6 @@
 # LAMBDAFunctions
 resource "aws_lambda_function" "this" {
-  function_name    = "nuke"
+  function_name    = "nuke-function"
   handler          = "index.test"
   runtime          = "nodejs16.x"
   role             = aws_iam_role.lambda.arn
