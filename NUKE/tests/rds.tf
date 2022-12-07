812091e7ffe156af6bbcf67378121149fe322dec
rex <81349047+cjsrkd3321@users.noreply.github.com>
2022-12-08 07:56:14 +0900

    Modify rds subnet

--- NUKE/tests/rds.tf
+++ NUKE/tests/rds.tf
@@ -1,7 +1,7 @@
 resource "aws_rds_cluster" "mysql" {
   cluster_identifier      = "nuke-aurora-mysql-cluster"
   engine                  = "aurora-mysql"
-  availability_zones      = ["${var.region}a", "${var.region}b"]
+  db_subnet_group_name    = aws_db_subnet_group.this.name
   database_name           = "mydb"
   master_username         = "foo"
   master_password         = "nuke-rex.chun"
@@ -12,10 +12,15 @@ resource "aws_rds_cluster" "mysql" {
 resource "aws_rds_cluster" "postgresql" {
   cluster_identifier      = "nuke-aurora-postgresql-cluster"
   engine                  = "aurora-postgresql"
-  availability_zones      = ["${var.region}a", "${var.region}b"]
+  db_subnet_group_name    = aws_db_subnet_group.this.name
   database_name           = "mydb"
   master_username         = "foo"
   master_password         = "nuke-rex.chun"
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
+}
+
+resource "aws_db_subnet_group" "this" {
+  name       = "main"
+  subnet_ids = [aws_subnet.this.id, aws_subnet.this2.id, aws_subnet.this3.id]
 }
