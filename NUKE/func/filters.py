2c75d6058b66b30262e3c8d93b24884b906e43b4
rex <81349047+cjsrkd3321@users.noreply.github.com>
2022-12-09 08:15:48 +0900

    Add is_create_date_less_than_now filter

--- NUKE/func/filters.py
+++ NUKE/func/filters.py
@@ -1,5 +1,15 @@
+from os import environ
+from datetime import datetime
 from resources.utils import has_value_from_tags
 
+import time
+import pytz
+
+environ["TZ"] = "UTC"
+utc = pytz.UTC
+time.tzset()
+
+now = utc.localize(datetime.now())
 
 # identifies untagged resources
 def have_tags(resource):
@@ -12,3 +22,24 @@ def have_no_nuke_project_tag(resource):
     if "tags" not in resource:
         return False
     return not has_value_from_tags(resource.get("tags"), "Project", "nuke")
+
+
+def is_create_date_less_than_now(resource):
+    if "create_date" not in resource:
+        return False
+    cd = resource["create_date"]
+    if type(cd) == int:
+        cd = datetime.fromtimestamp(cd / 1_000)
+    elif type(cd) == str:
+        try:
+            cd = datetime.fromtimestamp(int(cd))
+        except ValueError:
+            if cd.endswith("Z"):
+                cd = datetime.strptime(cd, "%Y-%m-%dT%H:%M:%S.%fZ")
+            else:
+                cd = datetime.strptime(cd, "%Y-%m-%d %H:%M:%S.%f")
+    if not cd.tzinfo:
+        cd = utc.localize(cd)
+    cd = cd.replace(tzinfo=utc)
+
+    return cd < now
