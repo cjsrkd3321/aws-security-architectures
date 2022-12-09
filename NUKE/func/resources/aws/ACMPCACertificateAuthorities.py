8f4b7191804b6dfce2694fae33b00c0cfbfb14f6
rex <81349047+cjsrkd3321@users.noreply.github.com>
2022-12-09 23:13:58 +0900

    Add ACMPCACertificateAuthorities

---
+++ NUKE/func/resources/aws/ACMPCACertificateAuthorities.py
@@ -0,0 +1,73 @@
+from resources import resources
+from resources.base import ResourceBase
+
+
+class ACMPCACertificateAuthority(ResourceBase):
+    def __init__(self, sess=None, default_filter_func=None):
+        self.svc = sess
+        self.exceptions = self.svc.exceptions
+        self.filter_func = default_filter_func
+
+    def list(self):
+        results = []
+
+        # Check if self.svc create from acm not acm-pca
+        if "list_certificate_authorities" not in dir(self.svc):
+            return results
+
+        try:
+            iterator = self.svc.get_paginator("list_certificate_authorities").paginate()
+            cas = [ca for cas in iterator for ca in cas["CertificateAuthorities"]]
+            for ca in cas:
+                arn = ca["Arn"]
+                try:
+                    tags = self.svc.list_tags(CertificateAuthorityArn=arn)["Tags"]
+                except (
+                    self.exceptions.ResourceNotFoundException,
+                    self.exceptions.InvalidStateException,
+                ):
+                    continue
+                results.append(
+                    {
+                        "id": arn,
+                        "name": arn,
+                        "tags": tags,
+                        "arn": arn,
+                        "state": ca["Status"],
+                        "create_date": ca["CreatedAt"],
+                        "last_modified_date": ca.get("LastStateChangeAt"),
+                    }
+                )
+            return results, None
+        except Exception as e:
+            return results, e
+
+    def remove(self, resource):
+        try:
+            self.svc.delete_certificate_authority(
+                CertificateAuthorityArn=resource["id"], PermanentDeletionTimeInDays=7
+            )
+            return True, None
+        except self.exceptions.ResourceNotFoundException:
+            return True, None
+        except Exception as e:
+            return False, e
+
+    def filter(self, resource, *filters):
+        if self.filter_func:
+            try:
+                if self.filter_func(resource):
+                    return self.filter_func.__name__, None
+            except Exception as e:
+                return False, e
+        for filter in filters:
+            if filter(resource):
+                return filter.__name__, None
+        return False, None
+
+    def properties(self):
+        pass
+
+
+if __name__ != "__main__":
+    resources.append(ACMPCACertificateAuthority)
--- NUKE/tests/acm.tf
+++ NUKE/tests/acm.tf
@@ -6,3 +6,15 @@ resource "aws_acm_certificate" "this" {
     create_before_destroy = true
   }
 }
+
+# # ACMPCACertificateAuthorities
+# resource "aws_acmpca_certificate_authority" "this" {
+#   certificate_authority_configuration {
+#     key_algorithm     = "RSA_4096"
+#     signing_algorithm = "SHA512WITHRSA"
+#     subject {
+#       common_name = "nuke-acmpca-certificates.com"
+#     }
+#   }
+#   permanent_deletion_time_in_days = 7
+# }
