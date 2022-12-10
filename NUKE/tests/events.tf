b9871cb4908c16f821c001a6ff3296331d8f86dc
rex <81349047+cjsrkd3321@users.noreply.github.com>
2022-12-10 21:01:39 +0900

    Add test for EVENTRules

---
+++ NUKE/tests/events.tf
@@ -0,0 +1,20 @@
+# EVENTSRules
+resource "aws_cloudwatch_event_rule" "this" {
+  name        = "capture-aws-sign-in"
+  description = "Capture each AWS Console Sign In"
+
+  event_pattern = <<EOF
+{
+  "detail-type": [
+    "AWS Console Sign In via CloudTrail"
+  ]
+}
+EOF
+}
+
+# EVENTSRules
+resource "aws_cloudwatch_event_target" "this" {
+  rule      = aws_cloudwatch_event_rule.this.name
+  target_id = "SendToSNS"
+  arn       = aws_sns_topic.aws_logins.arn
+}
--- NUKE/tests/sns.tf
+++ NUKE/tests/sns.tf
@@ -2,3 +2,14 @@
 resource "aws_sns_topic" "this" {
   name = "nuke-topic"
 }
+
+# SNSTopics
+resource "aws_sns_topic" "aws_logins" {
+  name = "nuke-aws-console-logins"
+}
+
+# SNSTopics
+resource "aws_sns_topic_policy" "this" {
+  arn    = aws_sns_topic.aws_logins.arn
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
+}
