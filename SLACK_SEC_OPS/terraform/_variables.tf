6c3bcc275c3e956585472067b7081b8d0578c64e
rex <81349047+cjsrkd3321@users.noreply.github.com>
2022-12-18 22:53:41 +0900

    Init commit

---
+++ SLACK_SEC_OPS/terraform/_variables.tf
@@ -0,0 +1,14 @@
+variable "slack_signing_secret" {
+  type      = string
+  sensitive = true
+}
+
+# variable "slack_webhook_url" {
+#   type      = string
+#   sensitive = true
+# }
+
+variable "slack_oauth_token" {
+  type      = string
+  sensitive = true
+}
